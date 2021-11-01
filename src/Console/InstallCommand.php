8db52b2199aa31a1973cfb2b7e12b3cf6e421f11
Nerijus <nerijus.bukartas@gmail.com>
2021-11-01 09:44:19 +0200

    Create js folder for plainadmin

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -253,6 +253,7 @@ class InstallCommand extends Command
 
         // Assets
         (new Filesystem)->ensureDirectoryExists(resource_path('js'));
+        (new Filesystem)->ensureDirectoryExists(public_path('js'));
         (new Filesystem)->ensureDirectoryExists(public_path('css'));
         (new Filesystem)->ensureDirectoryExists(public_path('fonts'));
         (new Filesystem)->ensureDirectoryExists(public_path('images'));
