1b7f221cb7509bba6e120d64bd4506c4a5df031e
Umer Farooq <zerokool_007@yahoo.com>
2021-11-15 23:29:42 +0400

    Email old variable name corrected, an extra wrap div removed from password and confirm password

--- resources/stubs/ui/voltbs5/views/auth/register.blade.php
+++ resources/stubs/ui/voltbs5/views/auth/register.blade.php
@@ -40,52 +40,49 @@
                                     </svg>
                                 </span>
                                 <input name="email" id="email" type="email" class="form-control"
-                                    placeholder="{{ __('Email') }}" value="{{ old('name') }}" autofocus required>
+                                    placeholder="{{ __('Email') }}" value="{{ old('email') }}" autofocus required>
                             </div>
                             @error('email') <div class="invalid-feedback"> {{ $message }} </div> @enderror
                         </div>
                         <!-- End of Form -->
-                        <div class="form-group">
-                            <!-- Form -->
-                            <div class="form-group mb-4">
-                                <label for="password">{{ __('Your Password') }}</label>
-                                <div class="input-group">
-                                    <span class="input-group-text" id="basic-addon2">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                                clip-rule="evenodd">
-                                            </path>
-                                        </svg>
-                                    </span>
-                                    <input name="password" type="password" placeholder="{{ __('Password') }}"
-                                        class="form-control" id="password" required autocomplete="new-password">
-                                </div>
-                                @error('password') <div class="invalid-feedback"> {{ $message }} </div>
-                                @enderror
+                        <!-- Form -->
+                        <div class="form-group mb-4">
+                            <label for="password">{{ __('Your Password') }}</label>
+                            <div class="input-group">
+                                <span class="input-group-text" id="basic-addon2">
+                                    <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                            clip-rule="evenodd">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <input name="password" type="password" placeholder="{{ __('Password') }}" class="form-control" id="password"
+                                    required autocomplete="new-password">
                             </div>
-                            <!-- End of Form -->
-                            <!-- Form -->
-                            <div class="form-group mb-4">
-                                <label for="password_confirmation">{{ __('Confirm Password') }}</label>
-                                <div class="input-group">
-                                    <span class="input-group-text" id="basic-addon3">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                                clip-rule="evenodd">
-                                            </path>
-                                        </svg>
-                                    </span>
-                                    <input name="password_confirmation" type="password"
-                                        placeholder="{{ __('Confirm Password') }}" class="form-control"
-                                        id="password_confirmation" required>
-                                </div>
-                            </div>
-                            <!-- End of Form -->
+                            @error('password') <div class="invalid-feedback"> {{ $message }} </div>
+                            @enderror
                         </div>
+                        <!-- End of Form -->
+                        <!-- Form -->
+                        <div class="form-group mb-4">
+                            <label for="password_confirmation">{{ __('Confirm Password') }}</label>
+                            <div class="input-group">
+                                <span class="input-group-text" id="basic-addon3">
+                                    <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                            clip-rule="evenodd">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <input name="password_confirmation" type="password" placeholder="{{ __('Confirm Password') }}"
+                                    class="form-control" id="password_confirmation" required>
+                            </div>
+                        </div>
+                        <!-- End of Form -->
                         <div class="d-grid">
                             <button type="submit" class="btn btn-gray-800">{{ __('Register') }}</button>
                         </div>
