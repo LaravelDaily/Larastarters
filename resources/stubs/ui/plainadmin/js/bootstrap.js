28674cbf2bad81cd53c22bf64438b18d3abd0a8c
Nerijus <nerijus.bukartas@gmail.com>
2022-05-15 09:57:13 +0300

    Fix plainadmin dropdown

--- resources/stubs/ui/plainadmin/js/bootstrap.js
+++ resources/stubs/ui/plainadmin/js/bootstrap.js
@@ -8,7 +8,6 @@ window._ = require('lodash');
 
 try {
     window.Popper = require('@popperjs/core').default;
-    window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
 } catch (e) {}
