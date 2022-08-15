25ba2250427561c09192b60bfbce1dc5712a3d71
André Santos <andrefilsantos@gmail.com>
2022-08-15 17:19:19 +0100

    Tailwindcomponents breeze version

--- resources/stubs/breeze/tailwindcomponents/views/layouts/app.blade.php
+++ resources/stubs/breeze/tailwindcomponents/views/layouts/app.blade.php
@@ -10,11 +10,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
