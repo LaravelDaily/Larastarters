3754a7248300c2e288050c6af1208a581ac5845a
Nerijus <krekas@gmail.com>
2022-10-14 11:41:11 +0300

    Fix windmill theme error when building assets

--- resources/stubs/breeze/windmill/tailwind.config.js
+++ resources/stubs/breeze/windmill/tailwind.config.js
@@ -1,9 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const plugin = require("tailwindcss/plugin");
-const Color = require("color");
 
 module.exports = {
-    purge: [
+    content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
