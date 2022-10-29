5070854e92c18e7a2323aff6702b0ac26dd87c63
Nerijus <krekas@gmail.com>
2022-10-29 21:25:50 +0300

    Replace styles file location in vite config

--- resources/stubs/ui/vite.config.js
+++ resources/stubs/ui/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
