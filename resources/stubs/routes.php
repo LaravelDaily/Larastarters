87747b8811a3132c2f2cdf06be3de7e9826d27f9
Md. Rakibul Islam <44586913+rakibdevs@users.noreply.github.com>
2023-03-01 15:05:35 +0600

    Removed profile destroy route

--- resources/stubs/routes.php
+++ resources/stubs/routes.php
@@ -30,7 +30,6 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
