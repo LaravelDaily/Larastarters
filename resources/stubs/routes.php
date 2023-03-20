0536b2c5a70d651e5d6d6f333e4a8dca83482d2d
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2023-03-20 16:45:31 +0530

    Update routes.php
    Change in routes.php because short routes using the controller function are easy to understand and easy to implement 

--- resources/stubs/routes.php
+++ resources/stubs/routes.php
@@ -25,12 +25,13 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
-
     Route::get('users', [UserController::class, 'index'])->name('users.index');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('profile', 'edit')->name('profile.edit');
+        Route::patch('profile', 'update')->name('profile.update');
+        Route::delete('profile', 'destroy')->name('profile.destroy');    
+    });
 });
 
 require __DIR__.'/auth.php';
